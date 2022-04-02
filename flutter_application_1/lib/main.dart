d57719679843816184d163f61c4a7de022968dba
Ryan Jonathan S <94353686+mike42s@users.noreply.github.com>
2022-04-02 18:11:34 +0700

    C2

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imagesList = [
+  'https://cdn.pixabay.com/photo/2020/11/01/23/22/breakfast-5705180_1280.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/19/00/breads-1836411_1280.jpg',
+  'https://cdn.pixabay.com/photo/2019/01/14/17/25/gelato-3932596_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/04/04/18/07/ice-cream-2202561_1280.jpg',
+];
+final List<String> titles = [
+  ' Coffee ',
+  ' Bread ',
+  ' Gelato ',
+  ' Ice Cream ',
+];
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,7 +31,7 @@ class MyApp extends StatelessWidget {
         ),
         home: SplashScreen(
           seconds: 2,
-          navigateAfterSeconds: new SecondScreen(),
+          navigateAfterSeconds: new _MyAppState(),
           title: new Text(
             'SplashScreen Example',
             style: new TextStyle(
@@ -28,6 +42,9 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.lightBlue[200],
         ));
   }
+
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
 class SecondScreen extends StatelessWidget {
@@ -40,6 +57,98 @@ class SecondScreen extends StatelessWidget {
         "Home page",
         textScaleFactor: 2,
       )),
+    );
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Carousel with indicator demo'),
+        ),
+        body: Column(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: false,
+                //enlargeCenterPage: true,
+                //scrollDirection: Axis.vertical,
+                onPageChanged: (index, reason) {
+                  setState(
+                    () {
+                      _currentIndex = index;
+                    },
+                  );
+                },
+              ),
+              items: imagesList
+                  .map(
+                    (item) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        margin: EdgeInsets.only(
+                          top: 10.0,
+                          bottom: 10.0,
+                        ),
+                        elevation: 6.0,
+                        shadowColor: Colors.redAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30.0),
+                          ),
+                          child: Stack(
+                            children: <Widget>[
+                              Image.network(
+                                item,
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              ),
+                              Center(
+                                child: Text(
+                                  '${titles[_currentIndex]}',
+                                  style: TextStyle(
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.bold,
+                                    backgroundColor: Colors.black45,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imagesList.map((urlOfItem) {
+                int index = imagesList.indexOf(urlOfItem);
+                return Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: _currentIndex == index
+                        ? Color.fromRGBO(0, 0, 0, 0.8)
+                        : Color.fromRGBO(0, 0, 0, 0.3),
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
